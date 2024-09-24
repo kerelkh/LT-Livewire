ae8ed36ae53bceb5eaf2c279d3024341129e1f28
kerelkh <kerelkh@gmail.com>
2024-09-24 08:59:04 +0700

    add preline frontend library and alpine js

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\About;
 use App\Livewire\Contact;
 use App\Livewire\Home;
+use App\Livewire\Login;
 use App\Livewire\Posts\Show;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,4 @@ Route::get('/', Home::class)->name('home');
 Route::get('/about', About::class)->name('about');
 Route::get('/contact', Contact::class)->name('contact');
 Route::get('/posts/show', Show::class)->name('posts.show');
+Route::get('/login', Login::class)->name('login');
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
+  darkMode: 'class',
 }
 
